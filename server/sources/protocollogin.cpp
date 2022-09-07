a029bab108e8e907d68b60ad55184aa68b715568
Claudio Carvalho <carvalhooclaudio@gmail.com>
2022-09-07 02:10:21 +0100

    delete unecessary import

--- server/sources/protocollogin.cpp
+++ server/sources/protocollogin.cpp
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ////////////////////////////////////////////////////////////////////////
 #include "otpch.h"
-#include "bcrypt.h"
 #include <iomanip>
 
 #include "protocollogin.h"
