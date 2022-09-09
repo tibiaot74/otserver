8dc8c91857f99d2d897da26308d1b348f0726024
Felipe Trindade <52676348+felipelaptrin@users.noreply.github.com>
2022-09-09 20:44:42 -0300

    remove infrastructure (#31)

--- infrastructure/terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  public_ip = aws_eip.this.public_ip
-  instance_id = (
-    var.use_spot == true ?
-    aws_spot_instance_request.this[0].spot_instance_id :
-    aws_instance.this[0].id
-  )
-}
--- infrastructure/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_server_id" {
-  description = "Instance ID of the Tibia Otserver"
-  value       = local.instance_id
-}
-
-output "public_ip" {
-  description = "Public IP of the Tibia Otserver"
-  value       = local.public_ip
-}
